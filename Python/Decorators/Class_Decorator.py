c0cfb0bf2a4978219c7458d70bc5892d0b701aef
PAVEL ROSTOV <mr.deff@gmail.com>
2020-03-16 16:56:38 +0000

    отлажен декоратор класса. !!! Не работает декоратор функции

--- Python/Decorators/Class_Decorator.py
+++ Python/Decorators/Class_Decorator.py
@@ -1,66 +1,78 @@
-# print("Hello, world!!")
 import functools
 import time 
 
 class TimeItCritical:
-    def __init__(self, obj=None,* , critical_time=0.5):
+    def __init__(self, obj=None, * , critical_time=0.1):
         functools.update_wrapper(self, obj)
         self._cls = None
         self._func = None
+        self.critical_time = critical_time
+        # если обворачиваемый объект является классом
         if isinstance(obj, type(TimeItCritical)):
             self._cls = obj
-            self.wrap_all_methods()
+            # obj = self.wrap_all_methods()  
+        # если обворачиваемый объект является функцией
         if isinstance(obj, type(self.__init__)):
             self._func = obj
-        self.critical_time = critical_time
+        
 
     def __call__(self, *args, **kwargs):
+        arg = ', '.join(map(str,args))
+        kwarg = ', '.join([f", {k}:{v}" for k,v in kwargs.items()])
+        print(f"__call__({arg}{kwarg})")
         if len(args) == 1 and callable(args[0]):
             obj = args[0]
             functools.update_wrapper(self, obj)
             if isinstance(obj, type(TimeItCritical)):
                 self._cls = obj
-                return self.wrap_all_methods()
+                return self.wrap_all_methods() 
             if isinstance(obj, type(self.__init__)):
                 self._func = obj
                 return self
-        else:
-            # ts = time.time()
-            # res = self.func(*args, **kwargs)
-            # te = time.time()
-            # print(f"{self.func.__name__!r} executed by {te-ts:2.2f} sec")
-            return self.timeit_wrapper
+        else:            
+            if self._cls:
+                return self.wrap_all_methods()(*args, **kwargs)
+            else:
+                # return self.timeit
+                ts = time.time()
+                res = self._func(*args, **kwargs)
+                te = time.time()
+                print(f"{self._func.__name__!r} executed by {te-ts:2.2f} sec")
+                return self.timeit
 
-    def timeit_wrapper(self, *args, **kwargs):
-        ts = time.time()
-        res = self.func(*args, **kwargs)
-        te = time.time()
-        delta = te - ts
-        if delta > self.critical_time:
-            print(f"{self.func.__name__!r} executed slow: {delta:2.2f} sec")
-        return res
+    def timeit(self, method):
+        def timeit_wrapper(*args, **kwargs):
+            ts = time.time()
+            res = method(*args, **kwargs)
+            te = time.time()
+            delta = te - ts
+            if delta > self.critical_time:
+                print(f"{method.__name__!r} executed slow: {delta:2.2f} sec")
+            return res
+        return timeit_wrapper
     
     def wrap_all_methods(self):
         class FakeCls():
-            def __init__(cls, *args, **kwargs):
-                self.obj = self._cls(*args, **kwargs)
+            def __init__(cls, *cls_args, **cls_kwargs):
+                cls.obj = self._cls(*cls_args, **cls_kwargs)
 
-            def __getattribute__(self, stuff):
+            def __getattribute__(cls, atr):
                 try:
                     # ищем атрибут в родительском классе
-                    attrib = super().__getattribute__(stuff)
-                except AttributeError:
+                    attrib = super().__getattribute__(atr)                    
+                except AttributeError as e:
+                    # print(e)
                     pass
                 else:
                     # если нажодим то ничего не делаем, просто возвращаем его
                     return attrib
                 
                 # Ищем атрибут в задикарируемом объекте
-                attrib = self.obj.__getattribute__(stuff)
+                attrib = cls.obj.__getattribute__(atr)                
 
                 # Проверка на принадлежность атрибута методу
-                if isinstance(attrib, type(self.__init__)):
-                    return self.timeit_wrapper(attrib)
+                if isinstance(attrib, type(cls.__init__)):
+                    return self.timeit(attrib)
                 else:
                     return attrib
         return FakeCls    
@@ -73,8 +85,14 @@ class TimeItCritical:
 #         print(i, end="")
 
 
-@TimeItCritical(critical_time=0.3)
+# @TimeItCritical(critical_time=0.3)
+@TimeItCritical
 class Foo:
+    # def __init__(self, num=1):
+    #     self.num = num
+    def __init__(self):
+        pass
+
     def a(self):
         print("медленный метод начался")
         time.sleep(1.0)
@@ -85,3 +103,10 @@ class Foo:
 f = Foo()
 f.a()
 f.b()
+
+@TimeItCritical(critical_time=0.3)
+def foofoo(name):
+    time.sleep(1.0)
+    print(f"Hello, {name}")
+
+foofoo('Peter')
